59ae34ef8ee7d3c8269f05976c47c95a5a037f33
Akhand Pratap <132538768+4aps@users.noreply.github.com>
2023-09-23 05:10:43 +0000

    datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,30 @@
+"use strict" //treat as newer JS version
+
+let name = "Harry"; //string
+let number1 = "273"; //string as it is double quotes
+let number2 = 123; 
+
+console.log(typeof number2); //number
+
+//DATATYPES =>
+//bigint
+//String => "" 
+//boolean => true/false
+//number => up to 2^53
+//null => Standalone value
+//undefined
+//symbol => unique value
+//object => collection of named values defined to respected keys
+
+//OBJECTS =>
+/* 
+Booleans, Numbers, Strings can be objects (if defined with the new keyword)
+Dates, Maths, Arrays, Functions, Regular Expressions are always objects
+*/ 
+
+
+let pass = null;
+console.log(typeof pass); //object** important
+
+let year;
+console.log(typeof year); //undefined
