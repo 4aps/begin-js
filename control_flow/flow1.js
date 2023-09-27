3929a14bde76eec5dea794ac8652ca6f1a3d8b13
Akhand Pratap <132538768+4aps@users.noreply.github.com>
2023-09-27 00:26:42 +0000

    control_js

---
+++ control_flow/flow1.js
@@ -0,0 +1,118 @@
+// let anyName = 'Vikram Sarabhai';
+
+// if(anyName == true){
+//     console.log('Hello Ji, hello!');
+// }
+// else{
+//     console.log('Bye');
+// }
+
+
+// let balance  = 2400;
+// if(balance >=1200){
+//     console.log('Savings');
+// }
+// else{
+//     console.log('No savings');
+// }
+
+
+// let val = '';
+// if(val != 0){
+//     console.log('exe1');
+// }
+// else{
+//     console.log('exe2');
+// }
+
+
+// const anyYear = 1900;
+// if(anyYear % 100 == 0 && anyYear % 400 == 0){
+//     console.log('Leap year');
+// }
+// else if (anyYear % 100 != 0 && anyYear % 4 == 0) {  
+//     console.log('Leap year');   
+// } else {
+//     console.log('Not a leap year');
+// }
+
+
+// const anyNum = 18;
+// if (anyNum % 3 != 0 || anyNum % 2 != 0){
+//     console.log('Not divisible by 6');
+// }
+// else{
+//     console.log('divisible by 6');
+// }
+
+
+// let weekDays = 3;
+// switch (weekDays) {
+//     case 1:
+//         console.log('Monday');
+//         break;
+//     case 2:
+//         console.log('Tuesday');
+//         break;
+//     case 3:
+//         console.log('Wednesday');
+//         break;
+//     case 4:
+//         console.log('Thursday');
+//         break;
+//     case 5:
+//         console.log('Friday');
+//         break;
+//     case 6:
+//         console.log('Saturday');
+//         break;
+//     case 7:
+//         console.log('Sunday');
+//         break;
+//     default:
+//         console.log('Invalid day!');
+//         break;
+// }
+
+
+// falsy values: null, undefined, NaN, 0, false, -0, BigInt 0n, ""
+// truthy values: "0", 'false', " ", [], {}, function(){}
+
+
+// const anyArr = [];
+// if(anyArr.length === 0){
+//     console.log('Array is empty.');
+// }
+
+
+// const myObj = {};
+// if(Object.keys(myObj).length === 0){
+//     console.log('Empty object');
+// }
+
+
+//Nullish collescing operator(??): null / undefined
+
+
+// let val1;
+// val1 = 12 ?? 17;
+// console.log(val1);
+
+
+// let val2;
+// val2 = null ?? 17;
+// console.log(val2);
+
+
+// let val3;
+// val2 = undefined ?? 7;
+// console.log(val3);
+
+// let userEmail;
+// if(userEmail = null ?? console.log(`Enter your mail!`));
+
+//ternary operator
+
+// const age = 4;
+// (age >= 18) ? console.log('You can vote') : console.log('You cannot vote');
+
