3cfe2fecdc2553a51e1fe1d58bbb590b8d70d039
Akhand Pratap <132538768+4aps@users.noreply.github.com>
2023-09-27 00:28:56 +0000

    loops in js

---
+++ iteration/loops.js
@@ -0,0 +1,54 @@
+//for loop
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// for (let i = 1; i < 41; i++) {
+//     console.log(`Table of ${i}:`);
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`${i} * ${j} = ${i*j} `);
+//     }
+// }
+
+
+// let heroes = ['Batman', 'Superman', 'Spiderman', 'Flash', 'Ironman'];
+// for (let i = 0; i < heroes.length; i++) {
+//     const element = heroes[i];
+//     console.log(element);
+// }
+
+
+//break
+
+// for (let i = 0; i < 11; i++) {
+//     if(i === 5){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+//continue
+
+// for (let i = 0; i < 11; i++) {
+//     if(i === 5){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+//while loop
+
+// let num = 100;
+// while (num % 29 != 0){
+//     console.log(num);
+//     num--;
+// }
+// console.log(`${num} is divisible by 29.`);
+
+//do-while loop: first run then check
+
+ const val = 77;
+ do {
+     console.log(val);
+} while (val >= 100);
