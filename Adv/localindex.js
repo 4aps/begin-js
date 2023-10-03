dcab71747ad1df71ce6cf2f4b4379ed4d8aa30a1
Akhand Pratap <132538768+4aps@users.noreply.github.com>
2023-10-03 07:58:57 +0530

    Create localindex.js

---
+++ Adv/localindex.js
@@ -0,0 +1,32 @@
+//Using import:
+
+// import {add, diff} from './library.js';
+
+// console.log(add(4, 5), diff(5, 2));
+
+// console.log('Hello');
+
+// Using require:
+
+const lib = require('./library.js');
+//console.log(lib);
+
+
+const fs = require('fs');
+
+// const t1 = performance.now();
+// const textFile = fs.readFileSync('test.txt', 'utf-8'); //('filename', 'encoding')
+// console.log(textFile);
+
+// const t2 = performance.now();
+
+// fs.readFile('test.txt', 'utf-8', (error, text) =>{
+//     console.log(text);
+// });
+
+// console.log(lib.add(4, 5), lib.diff(5, 2));
+
+// const t3 = performance.now();
+
+// console.log(t3 - t1);
+// console.log(t3 - t2);
