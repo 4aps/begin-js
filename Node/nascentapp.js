0c1be3ae4048495bf916ed06bee357fd8a2bffc6
Akhand Pratap <132538768+4aps@users.noreply.github.com>
2023-10-07 22:55:30 +0530

    Update nascentapp.js

--- Node/nascentapp.js
+++ Node/nascentapp.js
@@ -1,18 +1,27 @@
 const expres = require('express');
 const server = expres();
+const mongoose = require('mongoose');
 
 const productRouter = require('./routes/productRout')
+
+//DB connection
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect('mongodb+srv://MarsCode:A16t74aapsXzmlpMDBzaq01@cluster0.ri9zmzs.mongodb.net/Ecommerce');
+    console.log('database started');
+}
+
 
 //body parser
 server.use(expres.json());
 server.use(expres.static('public'));
-// server.use(expres.static('default'));
-server.use('/api/v1', productRouter.router)
+server.use(expres.static('default'));
+server.use('/products', productRouter.router)
 
 
  
 
-server.listen(8888, () => {
+server.listen(8080, () => {
     console.log('server started..')
 });
 
