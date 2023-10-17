420ef410bdcfaff8dd9e90df1d26ab352951e119
Akhand Pratap <132538768+4aps@users.noreply.github.com>
2023-10-17 23:19:41 +0530

    Create fn.hs

---
+++ Adv/fn.hs
@@ -0,0 +1,25 @@
+console.log("This is my JavaScript Set up");
+
+// function employee(Name, Salary) {
+//     console.log(Name +" is having a job of $"+ Salary +".");
+// }
+// let name1 = "Ajay";
+// let name2 = "Shijin";
+// let name3 = "Kirron";
+// let name4 = "Abhay";
+// const salary1 = 10000;
+// const salary2 = 20000;
+// const salary3 = 30000;
+// const salary4 = 40000;
+// employee(name1, salary1);
+// employee(name2, salary2);
+// employee(name3, salary3);
+// employee(name4, salary4);
+
+function sum(a, b, c) {
+    d = a + b + c;
+    return d;
+}
+
+sum(1, 2, 3);
+console.log("The sum is "+  d);
